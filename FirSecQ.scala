6d6d3102fd8381aafc94f7199fcbd3c9f026b418
Nadeesha142 <92771178+Nadeesha142@users.noreply.github.com>
2022-07-20 12:23:55 +0530

    Add files via upload

---
+++ FirSecQ.scala
@@ -0,0 +1,34 @@
+object FirSecQ extends App
+    {
+        var j:Int=2;
+        var i:Int=2;
+        var k:Int=2;
+        var m:Int=5;
+        var n:Int=5;
+        var f:Float=12.0;
+        var g:Float=4.0;
+        var c:Char='X';
+
+        var x=k+12*m;
+        var y=m/j;
+        var z=n%j;
+        var s=m/j*j;
+        var q=f+10*5+g;
+        var r=(i+1)*5; // (++i*5)there will be an error,scala had not support for  ++ increment operator,because of that(++i=1+i)take like that
+
+         println(x);
+         println(y);
+         println(z);
+         println(s);
+         println(q);
+         println(r);
+     }
+
+   
+  
+
+ 
+ 
+ 
+
+
